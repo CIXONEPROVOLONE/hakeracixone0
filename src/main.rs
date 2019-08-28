4d6efedc17cb03a32b5fd24a346dafd170619b44
CIXONEPROVOLONE <54625734+CIXONEPROVOLONE@users.noreply.github.com>
2019-08-28 14:46:30 +0200

    EEEEEEEEEEEEEEEEEEEEEEEE

--- src/main.rs
+++ src/main.rs
@@ -17,18 +17,23 @@ fn main() {
     rocket::ignite().mount("/", routes![index, eee]).launch();
 }
 
-///Questa funzione viene eseguita quando c'è una richiesta nel sito
+///Questa funzione viene eseguita quando c'è una richiesta nel sito e nel parametro della query c'è chiave. ad esempio
+///https://hakeracixone.herokuapp.com?chiave=supersegreto
 #[get("/?<chiave>")]
 fn index(chiave:String) -> String {
+    ///prende il risultato chiamando la funzione in C
     let result = unsafe{checkPassword(chiave.to_string().as_bytes().as_ptr())};
     if result == 0 {
+        //se il risultato è 0 scrive CHAIVE SBAGLIATA
         String::from("CHIAVE SBAGLIATA!!!")
     } else {
+        //prende la chiave segreta e la scrive
         String::from(format!("Password segreta: {}", std::env::var("chiave").unwrap()))
     }
 }
 
-///Questa funzione viene eseguita quando c'è una richiesta nel sito
+///Questa funzione viene eseguita quando c'è una richiesta nel sito e non c'è il parametro chiave
+///ad esempio: https://hakeracixone.herokuapp.com
 #[get("/")]
 fn eee() -> &'static str {
     "inserisci una chiave segreta (?chiave = chiave)"
