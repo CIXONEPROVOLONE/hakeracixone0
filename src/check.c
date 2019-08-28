7b9c087aab7bd02226c3d0cdb3c3556147a6d14d
CIXONEPROVOLONE <54625734+CIXONEPROVOLONE@users.noreply.github.com>
2019-08-28 12:56:57 +0200

    eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee

---
+++ src/check.c
@@ -0,0 +1,9 @@
+#include <string.h>
+
+int checkPassword(const char *password)
+{
+    int result = 0;
+    char pass[50];
+    strcpy(pass, password);
+    return result;
+}
---
+++ src/main.rs
@@ -0,0 +1,36 @@
+#![feature(proc_macro_hygiene, decl_macro, link_args)]
+
+#[macro_use]
+extern crate rocket;
+extern crate rocket_contrib;
+
+use rocket_contrib::serve::StaticFiles;
+use std::env;
+
+#[link(name = "check.o")]
+extern "C" {
+    fn checkPassword(password: *const u8) -> i32;
+}
+
+
+fn main() {
+    println!("Hello, world!");
+    rocket::ignite().mount("/", routes![index, eee]).launch();
+}
+
+///Questa funzione viene eseguita quando c'è una richiesta nel sito
+#[get("/?<chiave>")]
+fn index(chiave:String) -> String {
+    let result = unsafe{checkPassword(chiave.to_string().as_bytes().as_ptr())};
+    if result == 0{
+        String::from("CHIAVE SBAGLIATA!!!")
+    } else {
+        String::from(format!("Password segreta: {}", std::env::var("chiave")))
+    }
+}
+
+///Questa funzione viene eseguita quando c'è una richiesta nel sito
+#[get("/")]
+fn eee() -> &'static str {
+    "inserisci una chiave segreta (?chiave = chiave)"
+}
